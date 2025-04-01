045e45093bfe5ca067e549987e2cf328e7c796e9
checkurcode <92620396+checkurcode@users.noreply.github.com>
2025-04-01 20:25:17 +0530

    fix-4 Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,4 @@
-import express from "express";
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello from Render Deployment!");
-});
-
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
